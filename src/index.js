8682c544160fc6b3e18e37aba4ab0a33cbe0bbae
Dhruv <dhruv.prajapati@simformsolutions.com>
2023-04-11 10:44:01 +0530

    Phase1 Completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 const moment = require('moment');
+const SHOP_SCHEDULE = require('./schedule');
+
+const timeFormat = 'hh:mm A';
+const now = moment();
+const today = now.format('ddd');
+
+const day = SHOP_SCHEDULE.find(day => day.day === today);
+let status = 'CLOSED';
+
+if (day) {
+    day.open = moment(day.open, timeFormat);
+    day.close = moment(day.close, timeFormat);
+
+    if (now.isBetween(day.open, day.close, undefined, '[)'))
+        status = 'OPEN';
+}
+
+console.log(`OUTPUT: ${status}`);
